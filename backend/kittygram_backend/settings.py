7587e05093708ac99772cd3ec2bbd291e1383d72
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-11-20 16:38:35 +0400

    Added allowed hosts in backend.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['62.84.120.94', 'yakittygram.ddns.net']
+ALLOWED_HOSTS = ['127.0.0.1', '62.84.120.94', 'yakittygram.ddns.net']
 
 
 INSTALLED_APPS = [
