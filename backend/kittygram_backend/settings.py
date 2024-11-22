fc750ee5cd8d44378220e452c77cfe33c36e236d
Nastya Makarova <nastasia.konovalova@yandex.ru>
2024-11-22 20:37:10 +0400

    Changes for allowed hosts variable.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'default')
 
 DEBUG = os.getenv('DEBUG', '').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('HOSTS', 'localhost').split(', ')
+ALLOWED_HOSTS = os.getenv('HOSTS', 'localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
